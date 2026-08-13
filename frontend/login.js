f28f1fe71167aec6ac6fbd8b05ec3f1ce393e6f2
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-08-13 17:16:05 +0530

    added Login Url here

--- frontend/login.js
+++ frontend/login.js
@@ -1,4 +1,15 @@
-email_input = document.querySelector(".email-input");
-password_input = document.querySelector(".password-input");
-login_btn = document.querySelector(".login-button");
+const email_input = document.querySelector(".email-input");
+const password_input = document.querySelector(".password-input");
+const login_btn = document.querySelector(".login-button");
+const form_data = new FormData();
+const LOGIN_URL = "http://127.0.0.1:8000/auth/login";
+
+
+form_data.append("username", email_input.value);
+form_data.append("password", email_input.value);
+
+login_btn.addEventListener("click", () => {
+    console.log(email_input.value);
+    console.log(password_input.value);
+});
 
