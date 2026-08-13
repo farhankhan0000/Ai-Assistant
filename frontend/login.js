11a8e797b063d76d7b199e43373b6f65679757cf
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-08-13 16:48:10 +0530

    Started working on the connection of backend and frontend

---
+++ frontend/login.js
@@ -0,0 +1,4 @@
+email_input = document.querySelector(".email-input");
+password_input = document.querySelector(".password-input");
+login_btn = document.querySelector(".login-button");
+
--- frontend/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("login-button").addEventListener("click", function (){
-    const email = document.getElementById("email").value()
-    const password = document.getElementById("password").value()
-    console.log(email, password)
-}) 
