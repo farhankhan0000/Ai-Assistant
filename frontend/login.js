91f9e424e3f4c209c8eb9a317244b8961e8977ff
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-08-13 17:21:36 +0530

    added fetch to connect to python

--- frontend/login.js
+++ frontend/login.js
@@ -8,8 +8,7 @@ const LOGIN_URL = "http://127.0.0.1:8000/auth/login";
 form_data.append("username", email_input.value);
 form_data.append("password", email_input.value);
 
-login_btn.addEventListener("click", () => {
-    console.log(email_input.value);
-    console.log(password_input.value);
+fetch(LOGIN_URL, {
+    method: "post",
+    body: form_data
 });
-
