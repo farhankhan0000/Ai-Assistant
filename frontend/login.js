e21d96b63b2e04709d71715783718469b46db06d
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-08-13 18:33:02 +0530

    used async await to print the token in console

--- frontend/login.js
+++ frontend/login.js
@@ -8,15 +8,16 @@ const LOGIN_URL = "http://127.0.0.1:8000/auth/login";
 
 
 
-login_btn.addEventListener("click", () => {
+login_btn.addEventListener("click", async() => {
     const form_data = new URLSearchParams();
     form_data.append("username", email_input.value);
     form_data.append("password", password_input.value);
-    const jwt =  fetch(LOGIN_URL, {
+    const response =  await fetch(LOGIN_URL, {
     method: "post",
     body: form_data
 });
-    console.log(jwt);
+    const data = await response.json();
+    console.log(data);
 });
 
 
