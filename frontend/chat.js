2ba0b373de10c058527d027ae53c62b676a8d26a
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-08-19 21:28:40 +0530

    trying to create dynamic conversation on the layout

--- frontend/chat.js
+++ frontend/chat.js
@@ -6,16 +6,43 @@ const user_input = document.querySelector(".user-input");
 const send_btn = document.querySelector(".send-button");
 const user_msg = document.querySelector(".user-message");
 const ai_msg = document.querySelector(".ai-reply");
-const currentConversation_Id = null;
+const conversations_container = document.querySelector(".conversations");
+let currentConversation_Id = null;
 CHAT_URL = "http://127.0.0.1:8000/chat";
 CONVERSATION_URL = "http://127.0.0.1:8000/conversation";
+
+
+const create_conversation_button = (title, id) => {
+    const newButton = document.createElement("button");
+    newButton.classList.add("conversation");
+    newButton.innerText = title;
+    newButton.dataset.id = id;
+    conversations_container.appendChild(newButton);
+}
+
+new_chat_btn.addEventListener("click", async() => {
+    const token = document.
+    cookie.split("; ").find(row => row.startsWith("access_token="))?.split("=")[1];
+    const response = await fetch(CONVERSATION_URL, {
+        method: "POST",
+        headers: {
+            "content-Type" : "application/json",
+            "Authorization" : `Bearer ${token}`
+        },
+        body: JSON.stringify({title: "New Chat"})
+    });
+    const newConversation = await response.json();
+    currentConversation_Id = newConversation.conversation_id;
+    create_conversation_button(newConversation.title, currentConversation_Id);
+    console.log(newConversation);
+});
 
 
 
 send_btn.addEventListener("click", async () => {
     const chat_request = {
         content: user_input.value,
-        conversation_id: 1
+        conversation_id: currentConversation_Id
     };
     user_msg.innerText = user_input.value;
     const token = document.cookie.split("; ")
@@ -31,19 +58,4 @@ send_btn.addEventListener("click", async () => {
     const ai_reply = await response.json();
     ai_msg.innerText = ai_reply.ai_reply;
     console.log(ai_reply);
-});
-
-new_chat_btn.addEventListener("click", async() => {
-    const token = document.
-    cookie.split("; ").find(row => row.startsWith("access_token="))?.split("=")[1];
-    const response = await fetch(CONVERSATION_URL, {
-        method: "POST",
-        headers: {
-            "content-Type" : "application/json",
-            "Authorization" : `Bearer ${token}`
-        },
-        body: JSON.stringify({title: "New Chat"})
-    });
-    const newConversation = await response.json();
-    console.log(newConversation);
 });
--- frontend/login.js
+++ frontend/login.js
@@ -8,6 +8,7 @@ const LOGIN_URL = "http://127.0.0.1:8000/auth/login";
 
 
 
+
 login_btn.addEventListener("click", async() => {
     const form_data = new URLSearchParams();
     form_data.append("username", email_input.value);
