46a7750f18ab215f5cf686358b0dc9e252cbc26e
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-09-04 13:05:48 +0530

    fixed the deleted connection issue

--- assistant/database.py
+++ assistant/database.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 SQLALCHEMY_DATABASE_URL = ('postgresql+psycopg://neondb_owner:npg_WTLkHM6e8sdv@ep'
                            '-royal-sound-azid8qy6-pooler.c-3.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require')
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL,
+                       pool_pre_ping=True,
+                       pool_recycle=300)
 
 SessionLocal = sessionmaker(autocommit = False, autoflush= False, bind=engine)
 
