6ddd6bb609e45f1ff5922849f4d15757d5275a1a
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-09-05 23:35:55 +0530

    started testing the project

---
+++ frontend/signup.js
@@ -0,0 +1,39 @@
+const email = document.querySelector(".email-input");
+const name = document.querySelector(".name-input");
+const password = document.querySelector(".password-input")
+const signup_button = document.querySelector(".signup-btn")
+const SIGN_UP_URL = "http://localhost:8000/auth/register";
+
+
+signup_button.addEventListener("click", async () => {
+
+    const request_body = {
+        email: email.value,
+        name: name.value,
+        password: password.value
+    }
+
+    try {
+        const response = await fetch(SIGN_UP_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        credentials: "include",
+
+        body: JSON.stringify(request_body)
+        });
+
+        if(response.ok){
+            window.location.href = "login.html";
+        }else{
+            const errorData = await response.json();
+            alert(errorData.detail || "Signup Failed");
+        }
+
+    } catch (error){
+        console.error("Error during Signup:", error)
+    }
+
+
+});
