f552a90cc0155c6604949a33cd014d50a580ab12
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-09-08 17:08:15 +0530

    done with welcome page as well

--- frontend/welcome.js
+++ frontend/welcome.js
@@ -1,10 +1,15 @@
 const signupButton = document.querySelector(".signup-btn");
 const loginButton = document.querySelector(".login-btn");
 
+if(locationStorage.getItem("token")){
+    window.location.href="chat.html";
+}
+
 signupButton.addEventListener("click", () => {
-    window.location.href = "signup.html"
-})
+    window.location.href = "signup.html";
+});
 
 loginButton.addEventListener("click", () => {
-    window.location.href = "login.html"
-})
+    window.location.href = "login.html";
+});
+
