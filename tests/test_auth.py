4f30abb2c4b9499b640de86e4dc2e31f321eb0b9
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-09-10 23:33:08 +0530

    tested auth login and auth register and welcome page endpoints

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,15 +1,31 @@
 import uuid
+import pytest
 
 
-
-def test_successful_user_registration(client):
-    new_user = {
-        "email": f"test_{uuid.uuid4().hex[:8]}@exampl.com",
+@pytest.fixture()
+def new_user_payload():
+    return {
+        "email" : f"test_{uuid.uuid4().hex[:8]}@example.com",
         "name" : "Test_User",
         "password" : "weakpassword123"
     }
-    response = client.post("/auth/register", json=new_user)
+
+
+def test_successful_user_registration(client,new_user_payload):
+    response = client.post("/auth/register", json=new_user_payload)
 
     assert response.status_code == 201
 
+def test_successful_user_login(client, new_user_payload):
+    register_response = client.post("/auth/register", json=new_user_payload)
 
+    assert register_response.status_code == 201
+
+    login_data = {
+        "username" : new_user_payload["email"],
+        "password" : new_user_payload["password"]
+    }
+
+    response = client.post("/auth/login", data=login_data)
+    assert response.status_code == 200
+    assert "access_token" in response.json()
