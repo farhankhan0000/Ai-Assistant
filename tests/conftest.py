208634cb04515bb7b78f281797e6cb3c1fbad1cd
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-09-10 23:58:10 +0530

    switched to dummy memory database of sqlite for testing for avoiding crashesh

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,36 @@
 import pytest
 from fastapi.testclient import TestClient
 from assistant.main import app
+from sqlalchemy import create_engine, true
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.pool import StaticPool
+from assistant.database import Base, get_db
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread" : False},
+    poolclass = StaticPool
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+app.dependency_overrides[get_db] = override_get_db
+
+@pytest.fixture(autouse=True)
+def setup_database():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
 
 @pytest.fixture
 def client() ->TestClient:
