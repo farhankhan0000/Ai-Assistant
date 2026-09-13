be1d105b8d284c36b2ae95d6adb3ac18db26582f
farhan <mdfarhanahmadkhan7786@gmail.com>
2026-09-13 14:29:14 +0530

    updated database

--- assistant/database.py
+++ assistant/database.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URl")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL,
                        pool_pre_ping=True,
